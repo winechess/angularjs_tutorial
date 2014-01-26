a4ba2e8ab57c94958cdcb3ddb3ab5dae25db4b88
Sergey Vinichenko <vinichenko@imp-m.ru>
2014-01-26 23:57:43 +0400

    Add bootstrap css Add angularjs Create two-way binding Create e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,44 +2,15 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('AngularJS Tutorial app', function() {
 
   beforeEach(function() {
-    browser().navigateTo('app/index.html');
+    browser().navigateTo('AngularJSTutorial/index.html');
   });
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
+  it('should contains output input value', function() {
+      input("title").enter('Title');
+      expect(element('h2').html()).toContain("Title");
   });
 });
