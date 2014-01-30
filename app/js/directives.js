3eb8946ee598db55d158d371ed39a5af795b9a86
Sergey Vinichenko <winechess@gmail.com>
2014-01-31 01:48:28 +0400

    Create 2 directives which add mouse enter  and mouse leave behaviour to any component as it’s attribute; Add this attributes to all rows of the film table;

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,27 +6,18 @@ app.directive('film', function() {
     }
 });
 
-app.directive('filma', function() {
-    return{
-        restrict: 'A',
-        template: '<div>This is film-attr directive!!!</film>'
-
-    }
+app.directive('mouseenter', function(){
+   return function(scope, element){
+       element.bind('mouseenter', function(){
+           console.log("Cursor enter the element.")
+       });
+   } 
 });
 
-app.directive('filmc', function() {
-    return{
-        restrict: 'C',
-        template: '<div>This is film-class directive!!!</film>'
-
-    }
-});
-
-app.directive('filmcom', function() {
-    return{
-        restrict: 'M',
-        link: function(){
-            alert('Comment directive')
-        }
-    }
+app.directive('mouseleave', function(){
+   return function(scope, element){
+       element.bind('mouseleave', function(){
+           console.log('Cursor leave the element.')
+       });
+   } 
 });
